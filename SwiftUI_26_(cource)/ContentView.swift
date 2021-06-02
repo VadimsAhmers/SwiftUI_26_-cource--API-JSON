4a04d07a5f8d68e63fa3b11fc4b0331691e016fd
VadimsAhmers <vadimspring@mail.ru>
2021-06-02 09:56:05 +0500

    Initial Commit

---
+++ SwiftUI_26_(cource)/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI_26_(cource)
+//
+//  Created by Вадим Ахмеров on 02.06.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI_26_(cource)/SwiftUI_26__cource_App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_26__cource_App.swift
+//  SwiftUI_26_(cource)
+//
+//  Created by Вадим Ахмеров on 02.06.2021.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_26__cource_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
