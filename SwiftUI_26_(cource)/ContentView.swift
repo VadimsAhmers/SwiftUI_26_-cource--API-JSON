1a66946a897b5bc4fccb354d41af7006a4ee5135
VadimsAhmers <vadimspring@mail.ru>
2021-06-03 05:20:46 +0500

    Update ContentView.swift
    Нашел ошибку, исправил. Доделал урок 26.

--- SwiftUI_26_(cource)/ContentView.swift
+++ SwiftUI_26_(cource)/ContentView.swift
@@ -8,9 +8,48 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var posts: [Post] = []
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            List(posts) { post in
+                VStack {
+                    Text(post.title)
+                        .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+                    Text(post.body)
+                }
+            }
+            .onAppear() {
+                Api().getPost { (posts) in
+                    self.posts = posts
+                }
+            }
+            .navigationBarTitle("Posts")
+        }
+    }
+}
+
+
+struct Post: Codable, Identifiable {
+    let id = UUID()
+    var title: String
+    var body: String
+}
+
+class Api {
+    func getPost(completion: @escaping ([Post]) -> ()) {
+        guard let url = URL(string: "https://jsonplaceholder.typicode.com/posts")
+        else {
+            return
+        }
+        
+        URLSession.shared.dataTask(with: url) { (data, _, _) in
+            let posts = try! JSONDecoder().decode([Post].self, from: data!)
+            DispatchQueue.main.async {
+                completion(posts)
+            }
+        }
+        .resume()
     }
 }
 
